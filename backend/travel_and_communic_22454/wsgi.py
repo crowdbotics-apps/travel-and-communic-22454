bb34fd21a7845d31ab21d8a27ed09dde8f85aff1
Crowdbotics <team@crowdbotics.com>
2020-11-10 00:50:54 +0000

    Update models

--- backend/travel_and_communic_22454/wsgi.py
+++ backend/travel_and_communic_22454/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_and_communic_22454.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_and_communic_22454.settings")
 
 application = get_wsgi_application()
